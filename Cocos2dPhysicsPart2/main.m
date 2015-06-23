9f3e9bc1d5ca47ef17d828acada5087d53dbb44f
Vitaliy Voronok <kneo82@gmail.com>
2015-06-23 19:17:37 +0300

    Initial Commit

---
+++ Cocos2dPhysicsPart2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2dPhysicsPart2
+//
+//  Created by Vitaliy Voronok on 6/23/15.
+//  Copyright Vitaliy Voronok 2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
