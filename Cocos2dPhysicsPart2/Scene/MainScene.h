8137f0f3c8f978316819fff5bf47447a50ecc0f7
Vitaliy Voronok <kneo82@gmail.com>
2015-06-23 19:39:54 +0300

    Initial commit

---
+++ Cocos2dPhysicsPart2/Scene/MainScene.h
@@ -0,0 +1,16 @@
+//
+//  MainScene.h
+//  Cocos2dPhysicsPart2
+//
+//  Created by Vitaliy Voronok on 6/23/15.
+//  Copyright (c) 2015 Vitaliy Voronok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface MainScene : CCLayer
+
++ (CCScene *)scene;
+
+@end
